e0865c2f545fca5eaa567f47db4715744a82a953
Prerana888 <75772113+Prerana888@users.noreply.github.com>
2021-04-05 10:06:32 +0530

    Data Source

--- Tractor/src/main/java/com/xworkz/tractor/Tester.java
+++ Tractor/src/main/java/com/xworkz/tractor/Tester.java
@@ -14,8 +14,11 @@ public class Tester {
 		ApplicationContext context = new ClassPathXmlApplicationContext(contextConfiguration);
 
 		TractorDAO tractordao = context.getBean(TractorDAO.class);
-		TractorDTO tractordto = new TractorDTO("mahindra",5000.0,"red",50.0);
+//		TractorDTO tractordto = new TractorDTO("mahindra",5000.0,"red",50.0);
+		TractorDTO tractordto = new TractorDTO("Swaraj Tractor",900000.0,"black",70.0);
+		TractorDTO tractordto1 = new TractorDTO("tafe",860000.0,"green",79.0);
 		tractordao.saveTractor(tractordto);
+		tractordao.saveTractor(tractordto1);
 	}
 
 }
